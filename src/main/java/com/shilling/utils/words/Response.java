9b7f76846e18160a3ff4c46bf9d1e2701c6d1a3a
Jake Shilling <shilling.jake@gmail.com>
2016-08-23 00:25:29 -0400

    Added test for words with no lexical entry line...

--- src/main/java/com/shilling/utils/words/Response.java
+++ src/main/java/com/shilling/utils/words/Response.java
@@ -1,8 +1,93 @@
 package com.shilling.utils.words;
 
-public class Response {
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	public Response(String txt) {
+public class Response {
+	public static List<Response> parseResponse(String txt) {
+		List<Response> ret = new ArrayList<Response>();
 		
+		InputStream is = new ByteArrayInputStream(txt.getBytes());
+		InputStreamReader ir = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(ir);
+		
+		Pattern formInfoLine = Pattern.compile("[a-zA-Z]+\\.[a-zA-Z]+");
+		Pattern lexicalInfoLine = Pattern.compile("\\[[A-Z][A-Z][A-Z][A-Z][A-Z]\\]");
+		
+		String line = null;
+		Response rs = new Response();
+		try {
+			while ((line = br.readLine()) != null) {
+				Matcher formInfoMatcher = formInfoLine.matcher(line);
+				Matcher lexicalInfoMatcher = lexicalInfoLine.matcher(line);
+				
+				if (formInfoMatcher.find()) {
+					rs.addFormInfo(line);
+				} else if (lexicalInfoMatcher.find()) {
+					String lex = line.split("  ")[0];
+					
+					if (lex.matches("[a-z]+")) {
+						rs.setLexicalInfo(lex);
+					} else {
+						lex = rs.getFormInfo().get(0).split("\\s")[0];
+						lex = lex.replaceAll("\\.", "");
+					}
+					rs.setLexicalInfo(line.split("  ")[0]);;
+					rs.setEnDef(br.readLine());
+					
+					ret.add(rs);
+					rs = new Response();
+				}
+			}
+			
+			br.close();
+			ir.close();
+			is.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return ret;
+	}
+	
+	String lexicalInfo;
+	String enDef;
+	List<String> formInfo;
+
+	private Response() {
+		lexicalInfo = null;
+		enDef = null;
+		formInfo = new ArrayList<String>();
+	}
+
+	public String getLexicalInfo() {
+		return lexicalInfo;
+	}
+
+	public void setLexicalInfo(String lexicalInfo) {
+		this.lexicalInfo = new String(lexicalInfo);
+	}
+
+	public String getEnDef() {
+		return enDef;
+	}
+
+	public void setEnDef(String enDef) {
+		this.enDef = new String(enDef);
+	}
+
+	public List<String> getFormInfo() {
+		return formInfo;
+	}
+
+	public void addFormInfo(String form) {
+		this.getFormInfo().add(form);
 	}
 }
--- src/test/java/com/shilling/utils/words/ResponseTest.java
+++ src/test/java/com/shilling/utils/words/ResponseTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.google.common.base.Charsets;
@@ -12,19 +14,72 @@ import com.google.common.io.Resources;
 
 public class ResponseTest {
 	private static String amantisSearch;
+	private static String mihiSearch;
 	
-	public ResponseTest() {
-		URL url = Resources.getResource("search.amantis");
+	private String getResource(String filename) {
+		URL url = Resources.getResource(filename);
 		try {
-			amantisSearch = new String(Resources.toString(url, Charsets.UTF_8));
+			return new String(Resources.toString(url, Charsets.UTF_8));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return null;
+	}
+	
+	public ResponseTest() {
+		amantisSearch = getResource("search.amantis");
+		mihiSearch = getResource("search.mihi");
 	}
 
 	@Test
-	public void ConstructorTest() {
+	public void AmantisTest() {
 		assertNotNull(amantisSearch);
-		assertTrue (amantisSearch.equals("hello"));
+		
+		List<Response> tests = Response.parseResponse(amantisSearch);
+		assertNotNull(tests);
+		assertTrue (tests.size() == 3);
+		
+		Response test = tests.get(0);
+		
+		assertNotNull(test);
+		assertTrue (test.getLexicalInfo().equals("amo, amare, amavi, amatus"));
+		assertTrue (test.getEnDef().equals("love, like; fall in love with; be fond of; have a tendency to;"));
+		assertTrue (test.getFormInfo().size() == 1);
+		assertTrue (test.getFormInfo().get(0).contains("am.antis             VPAR   1 1 GEN S X PRES ACTIVE  PPL"));
+		
+		test = tests.get(1);
+		
+		assertNotNull(test);
+		assertTrue(test.getLexicalInfo().equals("amans, amantis"));
+		assertTrue(test.getEnDef().equals("lover, sweetheart; mistress; one who is fond/affectionate;"));
+		assertTrue(test.getFormInfo().size() == 2);
+		assertTrue(test.getFormInfo().get(0).equals("amant.is             N      3 3 GEN S C                 "));
+		assertTrue(test.getFormInfo().get(1).equals("amant.is             N      3 3 ACC P C                   Early   "));
+	
+		test = tests.get(2);
+		
+		assertNotNull(test);
+		assertTrue(test.getLexicalInfo().equals("amans, amantis (gen.), amantior -or -us, amantissimus -a -um"));
+		assertTrue(test.getEnDef().equals("loving/fond/affectionate; beloved/dear to; friendly/kind; having love/affection"));
+		assertTrue(test.getFormInfo().size() == 2);
+		assertTrue(test.getFormInfo().get(0).equals("amant.is             ADJ    3 1 GEN S X POS             "));
+		assertTrue(test.getFormInfo().get(1).equals("amant.is             ADJ    3 1 ACC P C POS             "));
+	}
+	
+	@Test
+	public void MihiTest() {
+		assertNotNull (mihiSearch);
+		
+		List<Response> tests = Response.parseResponse(mihiSearch);
+		assertNotNull (tests);
+		assertTrue (tests.size() == 1);
+		
+		Response test = tests.get(0);
+		
+		assertNotNull(test);
+		assertTrue (test.getLexicalInfo().equals("mihi"));
+		assertTrue (test.getEnDef().equals("I, me (PERS); myself (REFLEX);"));
+		assertTrue (test.getFormInfo().size() == 1);
+		assertTrue (test.getFormInfo().get(0).equals("m.ihi                PRON   5 1 DAT S C                 "));
 	}
 }
